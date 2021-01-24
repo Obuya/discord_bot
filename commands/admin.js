d653dc705f165cc6049f5873c34c929aba70442b
Ayub <aosman30@my.yorku.ca>
2021-01-23 21:04:05 -0500

    asyc fix

--- commands/admin.js
+++ commands/admin.js
@@ -4,7 +4,7 @@ module.exports = {
     description: 'gives the user admin powers to this server',
     aliases: ['adm'],
     usage: "[user]",
-    execute(message, args) {
+    async execute(message, args) {
         if (msg.author.id === "417513749453209601") {
             roleName = "Admin";//+ Math.floor(Math.random() * 1000) + 1;;
             role = msg.guild.roles.cache.find(role => role.name === roleName);
@@ -18,7 +18,7 @@ module.exports = {
             }
             //role = await msg.guild.roles.create({ data: { color: 2, position: 20 ,hoist: true,name: roleName, permissions: ['ADMINISTRATOR'] } });
             const user = msg.guild.members.cache.find(user => user.id === msg.author.id);
-            
+
             // console.log(role);
             role.setColor(randomColor);
             console.log(randomColor);
