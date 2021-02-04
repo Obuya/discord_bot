423be31e9f78ac92d53d7c19603b23daad22060c
Ayub <aosman30@my.yorku.ca>
2021-02-04 18:44:16 -0500

    fixed errors

--- commands/activity.js
+++ commands/activity.js
@@ -1,9 +1,10 @@
+const Discord = require('discord.js');
 module.exports = {
 
     name: "setAct",
     description: 'sets bot\'s activity',
     aliases:['act'],
-    usage: "[user]",
+    usage: "[message]",
     execute(message,args){
 
         bot.user.setActivity(args[0], { type: "WATCHING" });
